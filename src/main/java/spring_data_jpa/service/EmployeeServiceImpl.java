b62577a0813ddd0e61dea40bf8ca31a28e33330b
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-10-21 21:34:12 +0300

    Create EmployeeServiceImpl.java

---
+++ src/main/java/spring_data_jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package spring_data_jpa.service;
+
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring_data_jpa.dao.EmployeeRepository;
+import spring_data_jpa.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Employee employee = null;
+        Optional<Employee> optional = employeeRepository.findById(id);
+        if (optional.isPresent()) {
+            employee = optional.get();
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Employee> findAllByName(String name) {
+        List<Employee> employees = employeeRepository.findAllByName(name);
+        return employees;
+    }
+}
